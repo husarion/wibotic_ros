31a7b12d0dd7a00c3968cb5011c9d9e178349805
rafal-gorecki <rafal.gorecki@husarion.com>
2025-01-21 18:12:27 +0100

    Simplify external projects

--- wibotic_connector_can/cmake/SuperBuild.cmake
+++ wibotic_connector_can/cmake/SuperBuild.cmake
@@ -15,38 +15,27 @@
 include(ExternalProject)
 
 ExternalProject_Add(
-  ep_libuavcan
-  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/ep_libuavcan/upstream
-  INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-  GIT_REPOSITORY https://github.com/OpenCyphal-Garage/libcyphal/
-  GIT_TAG dcc3a4de237b7482e04543d2393c3a9385685312
-  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ep_libuavcan
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-  INSTALL_COMMAND make install INSTALL_PREFIX=<INSTALL_DIR>
-  UPDATE_DISCONNECTED 1
-  BUILD_IN_SOURCE 1
-  STEP_TARGETS build)
+  uavcan
+  GIT_REPOSITORY https://github.com/dronecan/libuavcan
+  GIT_TAG main
+  PREFIX uavcan
+  CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/uavcan
+)
+
 
 ExternalProject_Add(
-  ep_platform_specific_components
-  DEPENDS ep_libuavcan
+  linux_uavcan
+  DEPENDS uavcan
   GIT_REPOSITORY
     https://github.com/OpenCyphal-Garage/platform_specific_components/
   GIT_TAG 4745ef59f57b7e1c34705b127ea8c7a35e3874c1
-  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ep_platform_specific_components
+  PREFIX linux_uavcan
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND
     ${CMAKE_COMMAND} -E copy_directory
-    ${CMAKE_CURRENT_BINARY_DIR}/ep_platform_specific_components/src/ep_platform_specific_components/linux/libuavcan/include
-    ${CMAKE_INSTALL_PREFIX}/include)
+    ${CMAKE_BINARY_DIR}/linux_uavcan/src/linux_uavcan/linux/libuavcan/include
+    ${CMAKE_BINARY_DIR}/linux_uavcan/include)
 
-install(DIRECTORY ${INSTALL_DIR} DESTINATION ${CMAKE_INSTALL_PREFIX})
-
-ExternalProject_Add(
-  ep_wibotic_connector_can
-  DEPENDS ep_platform_specific_components
-  SOURCE_DIR ${PROJECT_SOURCE_DIR}
-  CMAKE_ARGS -DUSE_SUPERBUILD=OFF
-  INSTALL_COMMAND ""
-  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR})
+install(DIRECTORY ${CMAKE_BINARY_DIR}/uavcan ${CMAKE_BINARY_DIR}/linux_uavcan DESTINATION include)
